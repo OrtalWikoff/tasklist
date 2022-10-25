e7529720c0ab60545a989780d252e1ff5458a5b5
Ortal Wikoff <owikoff@gmail.com>
2022-10-25 12:17:09 -0400

    updated files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import AppFunction from './Container/AppFunction';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      < AppFunction />
+    </div>
+  );
+}
+
+export default App;
